50082617fad1dc479f4af33c033285e707737e34
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-02-08 20:22:58 -0300

    create shared components and style search button

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingRoutingModule } from './routing.module';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, RoutingRoutingModule],
+  imports: [CommonModule, RoutingRoutingModule, SharedModule],
 })
 export class PagesModule {}
