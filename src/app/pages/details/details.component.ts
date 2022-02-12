ad1589ff91341a4a324d670a4d2a2f0a47e8e2ae
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-02-12 10:43:32 -0300

    pokemon details page

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeAPIService } from 'src/app/service/poke-api.service';
+
+type Pokemon = any;
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  private pokemonURL: string = 'https://pokeapi.co/api/v2/pokemon';
+  private nameURL: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
-  constructor() { }
+  public pokemon: Pokemon;
+  public isLoaded = false;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeService: PokeAPIService
+  ) {}
 
   ngOnInit(): void {
+    this.getPokemon();
   }
 
+  public getPokemon() {
+    const pokemonId = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeService.getPokemonDetails(
+      `${this.pokemonURL}/${pokemonId}`
+    );
+    const name = this.pokeService.getPokemonDetails(
+      `${this.nameURL}/${pokemonId}`
+    );
+
+    forkJoin([pokemon, name]).subscribe((res) => {
+      this.pokemon = res;
+      this.isLoaded = true;
+    });
+  }
 }
